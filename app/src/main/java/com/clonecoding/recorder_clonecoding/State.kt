a3e5b606fc53a5331a2575315dd0002944902cb9
Onegold <ujini1129@gmail.com>
2021-09-05 21:07:12 +0900

    [feature] UI 구성 - State enum class 추가 - RecordButton custom view 추가 - RecordButton 벡터 이미지 파일 추가

---
+++ app/src/main/java/com/clonecoding/recorder_clonecoding/State.kt
@@ -0,0 +1,27 @@
+package com.clonecoding.recorder_clonecoding
+
+/**
+ * Recorder state enum class
+ */
+enum class State {
+
+    /**
+     * Before
+     */
+    BEFORE_RECORDING,
+
+    /**
+     * On recording
+     */
+    ON_RECORDING,
+
+    /**
+     * After
+     */
+    AFTER_RECORDING,
+
+    /**
+     * On playing
+     */
+    ON_PLAYING,
+}
